23296019c861d92045e29dc1214f26d906d55f29
Joram Bosire <jbosire@acl203.cs.vassar.edu>
2024-04-24 22:33:46 -0400

    fix lda code

--- lda.py
+++ lda.py
@@ -4,18 +4,28 @@ from gensim.models.ldamodel import LdaModel
 from nltk.tokenize import word_tokenize
 import pandas as pd
 
-doc = pd.read_csv('cleaned_readable.csv')
-processed_text = doc["processed_text"]
+def latent_dirichlet_allocation():
+    doc = pd.read_csv('cleaned_readable.csv')
+    processed_text = doc["processed_text"]
 
-tokens = [word_tokenize(str(sent)) for sent in processed_text]
+    tokens = [word_tokenize(str(sent)) for sent in processed_text]
 
-word_dict = corpora.Dictionary(tokens)
-text = [word_dict.doc2bow(token) for token in tokens] 
+    word_dict = corpora.Dictionary(tokens)
+    text = [word_dict.doc2bow(token) for token in tokens] 
 
-model = LdaModel(text, num_topics=5, id2word=word_dict, passes=15)
+    model = LdaModel(text, num_topics=5, id2word=word_dict, passes=15)
 
 
-topics = model.print_topics(num_words=10)
+    topics = model.print_topics(num_words=10)
 
-for topic in topics:
-    print(topic)
+    for topic in topics:
+        print(topic)
+
+
+def main():
+    latent_dirichlet_allocation()
+   
+    
+    
+if __name__ == "__main__":
+    main()
