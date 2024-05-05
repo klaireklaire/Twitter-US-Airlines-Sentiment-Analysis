59338115f57d2db1100cdb50deb75eeb5999f3c8
klaireklaire <khlinph@gmail.com>
2024-05-05 01:41:01 -0400

    som cnn stuff

--- cnn.py
+++ cnn.py
@@ -1,41 +1,8 @@
-# import tensorflow as tf
-# from tf import keras
-
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Embedding, Conv1D, GlobalMaxPooling1D, Dense
-
-# model = Sequential([
-#     Embedding(input_dim=10000, output_dim=128, input_length=100),  # Assuming vocab size of 10,000 and input length of 100
-#     Conv1D(64, 5, activation='relu'),  # 64 filters and a kernel size of 5
-#     GlobalMaxPooling1D(),
-#     Dense(10, activation='relu'),
-#     Dense(1, activation='sigmoid')  # Output layer for binary classification
-# ])
-
-# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# from tensorflow.keras.preprocessing.text import Tokenizer
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-# texts = ["This is a positive example.", "This is a negative example."]  # Example texts
-# tokenizer = Tokenizer(num_words=10000)
-# tokenizer.fit_on_texts(texts)
-# sequences = tokenizer.texts_to_sequences(texts)
-# data = pad_sequences(sequences, maxlen=100)
-
-# import numpy as np
-
-# labels = np.array([1, 0])  # Example binary labels for the texts
-# model.fit(data, labels, epochs=10, validation_split=0.2)
-
-# # Assume `test_data` and `test_labels` are prepared in the same way as `data` and `labels`
-# test_loss, test_acc = model.evaluate(data, labels)
-# print(f"Test Accuracy: {test_acc}")
 import tensorflow as tf
 from nltk.tokenize import word_tokenize
 import pandas as pd
 import math
-
+import numpy as np
 from string import punctuation
 from os import listdir
 from numpy import array
@@ -44,21 +11,26 @@ from numpy import zeros
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Embedding, Conv1D, MaxPooling1D
+from tensorflow.keras.layers import Dense, Flatten, Embedding, Conv1D, MaxPooling1D, Dropout
+from sklearn.metrics import f1_score, confusion_matrix, precision_score, recall_score, classification_report
+from sklearn.utils.class_weight import compute_class_weight
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-doc = pd.read_csv('cleaned_readable.csv')
-processed_text = doc["processed_text"]
-sentiments = doc["airline_sentiment"]
+def read_and_process_data():
+    doc = pd.read_csv('cleaned_readable.csv')
+    processed_text = doc["processed_text"]
+    sentiments = doc["airline_sentiment"]
 
-processed_data = [(tweet, sentiment) for tweet, sentiment in zip(processed_text, sentiments) if isinstance(tweet, str)]
+    processed_data = [(tweet, sentiment) for tweet, sentiment in zip(processed_text, sentiments) if isinstance(tweet, str)]
 
 
-tokens = [(word_tokenize(str(data[0])), data[1]) for data in processed_data]
-words = [item for row in tokens for item in row[0]]
+    tokens = [(word_tokenize(str(data[0])), data[1]) for data in processed_data]
+    words = [item for row in tokens for item in row[0]]
 
-positive_tweets = [tweet for tweet, sentiment in tokens if sentiment == "positive"]
-negative_tweets = [tweet for tweet, sentiment in tokens if sentiment == "negative"]
-neutral_tweets = [tweet for tweet, sentiment in tokens if sentiment == "neutral"]
+    positive_tweets = [tweet for tweet, sentiment in tokens if sentiment == "positive"]
+    negative_tweets = [tweet for tweet, sentiment in tokens if sentiment == "negative"]
+    return positive_tweets, negative_tweets, words
 
 # save list to file
 def save_list(lines, filename):
@@ -71,9 +43,6 @@ def save_list(lines, filename):
 	# close file
 	file.close()
 
-# save tokens to a vocabulary file
-save_list(words, 'vocab.txt')
-
 # load doc into memory
 def load_doc(filename):
     # open the file as read only
@@ -84,25 +53,6 @@ def load_doc(filename):
     file.close()
     return text
 
-# load all docs in a directory
-# def process_docs(directory, vocab, is_trian):
-#     documents = list()
-#     # walk through all files in the folder
-#     for filename in listdir(directory):
-#         # skip any reviews in the test set
-#         if is_trian and filename.startswith('cv9'):
-#             continue
-#         if not is_trian and not filename.startswith('cv9'):
-#             continue
-#         # create the full path of the file to open
-#         path = directory + '/' + filename
-#         # load the doc
-#         doc = load_doc(path)
-#         # clean doc
-#         tokens = clean_doc(doc, vocab)
-#         # add to list
-#         documents.append(tokens)
-#     return documents
 
 def load_embedding(filename):
     # load embedding into memory, skip first line
@@ -131,71 +81,107 @@ def get_weight_matrix(embedding, vocab):
     return weight_matrix
 
 # load the vocabulary
-vocab_filename = 'vocab.txt'
-vocab = load_doc(vocab_filename)
-vocab = vocab.split()
-vocab = set(vocab)
-
-
-
+def load_vocab():
+    vocab_filename = 'vocab.txt'
+    vocab = load_doc(vocab_filename)
+    vocab = vocab.split()
+    vocab = set(vocab)
+    return vocab
 
 # load all training reviews
-split_index = math.floor(len(positive_tweets) * 0.7)
-train_positive_docs = positive_tweets[:split_index]
-test_positive_docs = positive_tweets[split_index:]
+def load_test_train_docs(positive_tweets, negative_tweets):
+    split_index = math.floor(len(positive_tweets) * 0.7)
+    train_positive_docs = positive_tweets[:split_index]
+    test_positive_docs = positive_tweets[split_index:]
 
-split_index = math.floor(len(negative_tweets) * 0.7)
-train_negative_docs = negative_tweets[:split_index]
-test_negative_docs = negative_tweets[split_index:]
+    split_index = math.floor(len(negative_tweets) * 0.7)
+    train_negative_docs = negative_tweets[:split_index]
+    test_negative_docs = negative_tweets[split_index:]
+    return train_positive_docs, test_positive_docs, train_negative_docs, test_negative_docs
 
-train_docs = train_negative_docs + train_positive_docs
-# print(train_docs)
-# create the tokenizer
-tokenizer = Tokenizer()
-# fit the tokenizer on the documents
-tokenizer.fit_on_texts(train_docs)
+def tokenize_and_encode(train_positive_docs, test_positive_docs, train_negative_docs, test_negative_docs):
+    train_docs = train_negative_docs + train_positive_docs
+    tokenizer = Tokenizer()
+    # fit the tokenizer on the documents
+    tokenizer.fit_on_texts(train_docs)
+    # sequence encode
+    encoded_docs = tokenizer.texts_to_sequences(train_docs)
+    # pad sequences
+    max_length = max([len(s) for s in train_docs])
+    Xtrain = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+    # define training labels
+    ytrain = array([0 for _ in range(6412)] + [1 for _ in range(1648)])
+    
+    # load all test reviews
+    test_docs = test_negative_docs + test_positive_docs
 
-# sequence encode
-encoded_docs = tokenizer.texts_to_sequences(train_docs)
-# pad sequences
-max_length = max([len(s.split()) for s in train_docs])
-Xtrain = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
-# define training labels
-ytrain = array([0 for _ in range(900)] + [1 for _ in range(900)])
+    # sequence encode
+    encoded_docs = tokenizer.texts_to_sequences(test_docs)
+    # pad sequences
+    Xtest = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+    # define test labels
+
+    ytest = array([0 for _ in range(2749)] + [1 for _ in range(707)])
+    
+    # define vocabulary size (largest integer value)
+    vocab_size = len(tokenizer.word_index) + 1
+    return tokenizer, Xtrain, ytrain, Xtest, ytest, vocab_size, max_length
+    
+def load_embed(tokenizer, vocab_size, max_length):
+    # load embedding from file
+    raw_embedding = load_embedding('glove.twitter.27B.100d.txt')
+    # get vectors in the right order
+    embedding_vectors = get_weight_matrix(raw_embedding, tokenizer.word_index)
+    # create the embedding layer
+    embedding_layer = Embedding(vocab_size, 100, weights=[embedding_vectors], input_length=max_length, trainable=False)
+    return embedding_layer
  
-# load all test reviews
-test_docs = test_negative_docs + test_positive_docs
+def define_and_evaluate_model(embedding_layer, Xtrain, ytrain, Xtest, ytest):
+    model = Sequential()
+    model.add(embedding_layer)
+    model.add(Conv1D(filters=128, kernel_size=5, activation='relu'))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
 
-# sequence encode
-encoded_docs = tokenizer.texts_to_sequences(test_docs)
-# pad sequences
-Xtest = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
-# define test labels
-ytest = array([0 for _ in range(100)] + [1 for _ in range(100)])
-print("this is ytest ", ytest)
- 
-# define vocabulary size (largest integer value)
-vocab_size = len(tokenizer.word_index) + 1
+    # compile network
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    
+    # fit network
+    model.fit(Xtrain, ytrain, epochs=10, verbose=2)
+    print(model.summary())
+    
+    y_pred = (model.predict(Xtest) > 0.5).astype("int32")
+    
+    print("Confusion Matrix:")
+    cm = confusion_matrix(ytest, y_pred)
+    print(classification_report(ytest, y_pred, target_names=['Negative', 'Positive']))
+    total_correct = cm.diagonal()
+    total_predictions = cm.sum(axis=1)
+    accuracy_per_class = total_correct / total_predictions
+    print(f"Accuracy for Negative: {accuracy_per_class[0]}")
+    print(f"Accuracy for Positive: {accuracy_per_class[1]}")
+    plt.figure(figsize=(10,7))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+    plt.title('Confusion Matrix')
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.show()
+    
+    return model
 
-# load embedding from file
-raw_embedding = load_embedding('glove.twitter.27B.25d.txt')
-# get vectors in the right order
-embedding_vectors = get_weight_matrix(raw_embedding, tokenizer.word_index)
-# create the embedding layer
-embedding_layer = Embedding(vocab_size, 100, weights=[embedding_vectors], input_length=max_length, trainable=False)
- 
-# define model
-model = Sequential()
-model.add(embedding_layer)
-model.add(Conv1D(filters=128, kernel_size=5, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
-model.add(Flatten())
-model.add(Dense(1, activation='sigmoid'))
-print(model.summary())
-# compile network
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# fit network
-model.fit(Xtrain, ytrain, epochs=10, verbose=2)
-# evaluate
-loss, acc = model.evaluate(Xtest, ytest, verbose=0)
-print('Test Accuracy: %f' % (acc*100))
+def main():
+    positive_tweets, negative_tweets, words = read_and_process_data()
+    # save tokens to a vocabulary file
+    save_list(words, 'vocab.txt')
+    train_positive_docs, test_positive_docs, train_negative_docs, test_negative_docs = load_test_train_docs(positive_tweets, negative_tweets)
+    tokenizer, Xtrain, ytrain, Xtest, ytest, vocab_size, max_length = tokenize_and_encode(train_positive_docs, test_positive_docs, train_negative_docs, test_negative_docs)
+    embedding_layer = load_embed(tokenizer, vocab_size, max_length)
+    model = define_and_evaluate_model(embedding_layer, Xtrain, ytrain, Xtest, ytest)
+  
+    
+    
+if __name__ == "__main__":
+    main()
